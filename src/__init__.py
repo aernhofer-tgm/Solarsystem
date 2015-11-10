7159417d440cd8d01b7dafa980607d73dc922b8d
Andreas Ernhofer <aernhofer@student.tgm.ac.at>
2015-11-10 15:03:56 +0100

    First init

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,25 @@
-__author__ = 'andie'
+__author__ = 'Andi Ernhofer'
+
+import pygame
+
+pygame.init()
+
+white = (255,255,255)
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Solarsystem')
+
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+
+    pygame.draw.rect(gameDisplay, white,[400,300,50,50])
+    pygame.display.update()
+
+pygame.quit()
+quit()
