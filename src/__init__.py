3b732ce91f03123fa638f8fa8528ab20e3fb4cc7
jkopec <jkopec@student.tgm.ac.at>
2015-11-14 17:58:58 +0100

    Erste Tests mit GLUT

--- src/__init__.py
+++ src/__init__.py
@@ -4,22 +4,46 @@ import pygame
 
 pygame.init()
 
+#define colors
 white = (255,255,255)
+black = (0,0,0)
 
-gameDisplay = pygame.display.set_mode((800,600))
+#define display size
+display_width = 1280
+display_height = 720
+
+#creating the game display
+screen = pygame.display.set_mode((display_width,display_height))
+
+#setting the name of the window
 pygame.display.set_caption('Solarsystem')
 
-pygame.display.update()
-
+#define variable for exiting game
 gameExit = False
 
+#define clock for setting frame rates
+clock = pygame.time.Clock()
+
+#create a while loop for as long as the game gets quitted
 while not gameExit:
     for event in pygame.event.get():
+        #what should happen if someone quitts the game?
         if event.type == pygame.QUIT:
+            #while ends after this point
             gameExit = True
 
-    pygame.draw.rect(gameDisplay, white,[400,300,50,50])
+    #paint the whole screen black to remove not wanted parts from any older frame
+    screen.fill(black)
+
+    #paint something
+
+    #update to see changes
     pygame.display.update()
 
+    #set frame rate to 30
+    clock.tick(30)
+
+#quitting pygame
 pygame.quit()
+#quitting python
 quit()
