3c73669c221c04e6cb858e03646b8920b5907162
Andreas Ernhofer <aernhofer@student.tgm.ac.at>
2015-12-07 21:29:50 +0100

    Bewegungsgrundlagen

--- src/__init2__.py
+++ src/__init2__.py
@@ -6,6 +6,20 @@ __author__ = 'Jakub Kopec'
 import pygame
 from pygame.locals import *
 
+"""
+STEUERUNG
+---------
+
+k           Kammera Ansicht ändern
+p           Pause
+.           Schneller
+,           langsamer
+Pfeiltasten Bewegen
+wasd        Ansicht ändern
++           Ranzoomen
+-           Wegzoomen
+"""
+
 pygame.init()
 
 #define display size
@@ -33,6 +47,8 @@ solarsystem = Solarsystem()
 #create a while loop for as long as the game gets quitted
 while not gameExit:
     keys = pygame.key.get_pressed()  #checking pressed keys
+
+    #Zoom
     if keys[pygame.K_PLUS]:
         if kamera.upY == 1:
             kamera.eyeZ -= 1
@@ -49,12 +65,47 @@ while not gameExit:
             kamera.eyeY += 1
             kamera.update()
 
-    if keys[pygame.K_s]:
+    #Geschwindigkeit
+    if keys[pygame.K_PERIOD]:
         solarsystem.updateSpeed(0.1)
 
-    if keys[pygame.K_l]:
+    if keys[pygame.K_COMMA]:
         if solarsystem.geschwindigkeit != 0:
             solarsystem.updateSpeed(-0.1)
+
+    #Bewegen
+    if keys[pygame.K_RIGHT]:
+        kamera.eyeX += 1
+        kamera.update()
+
+    if keys[pygame.K_LEFT]:
+        kamera.eyeX -= 1
+        kamera.update()
+
+    if keys[pygame.K_UP]:
+        kamera.eyeY += 1
+        kamera.update()
+
+    if keys[pygame.K_DOWN]:
+        kamera.eyeY -= 1
+        kamera.update()
+
+    #Blickwinkel
+    if keys[pygame.K_d]:
+        kamera.centerX += 1
+        kamera.update()
+
+    if keys[pygame.K_a]:
+        kamera.centerX -= 1
+        kamera.update()
+
+    if keys[pygame.K_w]:
+        kamera.centerY += 1
+        kamera.update()
+
+    if keys[pygame.K_s]:
+        kamera.centerY -= 1
+        kamera.update()
 
     for event in pygame.event.get():
         #what should happen if someone quitts the game?
